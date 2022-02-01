0eda381529650b07d01cd498b9965ab2982ba907
Angelica Lirio <angelicablirio@gmail.com>
2022-02-01 14:39:00 -0300

    Criação projeto 04

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './style.css';
+import Navbar from './componentes/Navbar/Navbar';
+import TaskList from './componentes/TaskList/TaskList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let idAcc = 0;
+const generateId = ()=>{
+  idAcc = idAcc +1;
+  return idAcc;
 }
 
-export default App;
+
+export default function App(){
+
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) =>{
+    const newTask ={
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks)=>{
+      return [...existingTasks, newTask];
+    });
+  };
+
+
+  const updateTask = (id, title, state)=>{
+    setTasks((existingTasks)=>{
+      return existingTasks.map((task)=>{
+        if(task.id === id) {
+          return{...task, title, state}
+        }else{
+          return task;
+        }
+      });
+    });
+  };
+
+  const deleteTask = (id) =>{
+    setTasks((existingTasks)=>{
+      return existingTasks.filter((task)=> task.id !== id);
+    });
+  };
+
+  return(
+    <div className='App'>
+      <Navbar/>
+      <div className='container'>
+        <TaskList 
+        title="Pendente" 
+        onAddTask={addTask} 
+        taskState="Pendente"
+        tasks={tasks.filter((t)=> t.state ==="Pendente")} 
+        onTaskUpdate={updateTask}
+        onDeleteTask={deleteTask}
+        />
+        <TaskList 
+        title="Fazendo" 
+        onAddTask={addTask} 
+        taskState="Fazendo"
+        tasks={tasks.filter((t)=> t.state ==="Fazendo")}
+        onTaskUpdate={updateTask}
+        onDeleteTask={deleteTask}
+        />
+        <TaskList 
+        title="Completa" 
+        onAddTask={addTask}
+        taskState="Completa" 
+        tasks={tasks.filter((t)=> t.state ==="Completa")}
+        onTaskUpdate={updateTask}
+        onDeleteTask={deleteTask}
+        />
+      </div>
+    </div>
+  )
+}
